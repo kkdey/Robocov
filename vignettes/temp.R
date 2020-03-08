512666a6437a548904755258de312bb065784b43
Kushal K Dey <kshldey@gmail.com>
2020-03-08 10:49:22 -0400

    added vignette images

--- vignettes/temp.R
+++ vignettes/temp.R
@@ -1,26 +1,32 @@
 
 data("sample_by_feature_data")
 standard = cor(sample_by_feature_data, use = "pairwise.complete.obs")
+png("/Users/kushaldey/Documents/Robocov/vignettes/standard.png", res = 600)
 corrplot::corrplot(standard, diag = TRUE,
                    col = colorRampPalette(c("lightblue4", "lightblue2", "white", "indianred1", "indianred3"))(200),
                    tl.pos = "ld", tl.cex = 0.2, tl.col = "black",
                    rect.col = "white",na.label.col = "white",
                    method = "color", type = "lower", tl.srt=45)
+dev.off()
 
 robocov = Robocov_cor(sample_by_feature_data)
 colnames(robocov) = colnames(standard)
 rownames(robocov) = rownames(standard)
+png("/Users/kushaldey/Documents/Robocov/vignettes/robocov.png", res = 600)
 corrplot::corrplot(robocov, diag = TRUE,
                    col = colorRampPalette(c("lightblue4", "lightblue2", "white", "indianred1", "indianred3"))(200),
                    tl.pos = "ld", tl.cex = 0.2, tl.col = "black",
                    rect.col = "white",na.label.col = "white",
                    method = "color", type = "lower", tl.srt=45)
+dev.off()
 
 probocov = Robocov_precision(sample_by_feature_data, alpha = 0.1)
 colnames(probocov) = colnames(standard)
 rownames(probocov) = rownames(standard)
+png("/Users/kushaldey/Documents/Robocov/vignettes/probocov.png", res = 600)
 corrplot::corrplot(probocov, diag = TRUE,
                    col = colorRampPalette(c("lightblue4", "lightblue2", "white", "indianred1", "indianred3"))(200),
                    tl.pos = "ld", tl.cex = 0.2, tl.col = "black",
                    rect.col = "white",na.label.col = "white",
                    method = "color", type = "lower", tl.srt=45)
+dev.off()
