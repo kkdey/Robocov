172f571aec09c5af0c62cb8119ccbe8402abdbf8
Kushal K Dey <kshldey@gmail.com>
2020-03-08 11:18:17 -0400

    added a vignette

--- vignettes/temp.R
+++ vignettes/temp.R
@@ -1,7 +1,7 @@
 
 data("sample_by_feature_data")
 standard = cor(sample_by_feature_data, use = "pairwise.complete.obs")
-png("/Users/kushaldey/Documents/Robocov/vignettes/standard.png", res = 600)
+png("/Users/kushaldey/Documents/Robocov/vignettes/standard.png", width = 4, height = 4, units = 'in', res = 2000)
 corrplot::corrplot(standard, diag = TRUE,
                    col = colorRampPalette(c("lightblue4", "lightblue2", "white", "indianred1", "indianred3"))(200),
                    tl.pos = "ld", tl.cex = 0.2, tl.col = "black",
@@ -12,7 +12,7 @@ dev.off()
 robocov = Robocov_cor(sample_by_feature_data)
 colnames(robocov) = colnames(standard)
 rownames(robocov) = rownames(standard)
-png("/Users/kushaldey/Documents/Robocov/vignettes/robocov.png", res = 600)
+png("/Users/kushaldey/Documents/Robocov/vignettes/robocov.png", width = 4, height = 4, units = 'in', res = 2000)
 corrplot::corrplot(robocov, diag = TRUE,
                    col = colorRampPalette(c("lightblue4", "lightblue2", "white", "indianred1", "indianred3"))(200),
                    tl.pos = "ld", tl.cex = 0.2, tl.col = "black",
@@ -23,7 +23,7 @@ dev.off()
 probocov = Robocov_precision(sample_by_feature_data, alpha = 0.1)
 colnames(probocov) = colnames(standard)
 rownames(probocov) = rownames(standard)
-png("/Users/kushaldey/Documents/Robocov/vignettes/probocov.png", res = 600)
+png("/Users/kushaldey/Documents/Robocov/vignettes/probocov.png", width = 4, height = 4, units = 'in', res = 2000)
 corrplot::corrplot(probocov, diag = TRUE,
                    col = colorRampPalette(c("lightblue4", "lightblue2", "white", "indianred1", "indianred3"))(200),
                    tl.pos = "ld", tl.cex = 0.2, tl.col = "black",
